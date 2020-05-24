4172c89fb31dc607ae2a3809b9b5dd4326a5fb43
LeRingue <42521105+LeRingue@users.noreply.github.com>
2020-05-24 09:15:31 +0200

    Eggman not squared

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var cardsArray = [
   {    'name': 'SONIC',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/SONIC.png',  },
-  {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
-  {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
-  {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
-  {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
-  {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
-  {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
-  {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
-  {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
-  {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
-  {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
-  {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
+  {    'name': 'SHADOW',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Shadow.bmp',  },
+  {    'name': 'STICKS,    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Sticks.bmp',  },
+  {    'name': 'AMYROSE',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/AmyRose.bmp',  },
+  {    'name': 'TAILS',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Tails.bmp',  },
+  {    'name': 'ZOOEY',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Zooey.bmp',  },
+  {    'name': 'KNUCKLES',    'img': https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Knuckles.bmp',  },
+  {    'name': 'PERCI',    'img': https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Perci.bmp',  },
+  {    'name': 'EGGMAN',    'img': 'https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Eggman.bmp',  },
+  {    'name': 'DAVE',    'img': https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Dave.bmp',  },
+  {    'name': 'CUBOT',    'img': https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Cubot.bmp',  },
+  {    'name': 'ORBOT',    'img': https://raw.githubusercontent.com/LeRingue/MatchSonic/master/img/Orbot.bmp',  },
 ];
 
 // Duplicate cardsArray to create a match for each card
